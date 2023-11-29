6e92b8f16808bf7169a52153d87b8c7ac9d7433e
NamasakaLennox <namasakalennox254@gmail.com>
2023-11-29 19:57:39 +0300

    [fix:] error handling

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  return Promise.allSettled([user, photo]).then((res) => res);
+    const output = Promise.allSettled([user, photo]).then((res) => res);
+    return Promise.resolve(output)
 }
