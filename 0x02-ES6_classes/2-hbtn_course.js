1767d961a5ee7bf4e692480c05e7cd53861ecccf
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-01 01:36:15 +0300

    use getters and setter attributes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  /* getter methods */
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  /* setter methods */
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = val;
+  }
+
+  set length(val) {
+    if (typeof val !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    this._length = val;
+  }
+
+  set students(val) {
+    if (!(val instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!val.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = val;
+  }
+}
