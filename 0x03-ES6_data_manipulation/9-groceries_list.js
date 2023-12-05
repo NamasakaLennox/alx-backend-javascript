5954f3465bf4caa9f35fa012916266806194e5d2
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-05 23:08:30 +0300

    returns a map of grocery list

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+const groceriesList = () => {
+  const newMap = new Map();
+
+  newMap.set('Apples', 10);
+  newMap.set('Tomatoes', 10);
+  newMap.set('Pasta', 1);
+  newMap.set('Rice', 1);
+  newMap.set('Banana', 5);
+
+  return newMap;
+};
+
+export default groceriesList;
